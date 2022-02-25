21ff87a40d8002426f33f91655d7404c683a5c67
kush597 <98517341+kush597@users.noreply.github.com>
2022-02-25 11:14:38 +0530

    Add files via upload

---
+++ 7.GOLANG-COMMON-VULNERABILITIES/7.9.1.Dead-Lock-Detection/7.9.1.DEAD_LOCK.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/y-taka-23/ddsv-go/deadlock"
+	"github.com/y-taka-23/ddsv-go/deadlock/rule"
+	"github.com/y-taka-23/ddsv-go/deadlock/rule/do"
+	"github.com/y-taka-23/ddsv-go/deadlock/rule/vars"
+	"github.com/y-taka-23/ddsv-go/deadlock/rule/when"
+)
+
+func main() {
+
+	philo := func(me int, left, right vars.Name) deadlock.Process {
+		return deadlock.NewProcess().
+			EnterAt("0").
+			// Pick up the fork on his left side
+			Define(rule.At("0").Only(when.Var(left).Is(0)).
+				Let("up_l", do.Set(me).ToVar(left)).MoveTo("1")).
+			// Pick up the fork on his right side
+			Define(rule.At("1").Only(when.Var(right).Is(0)).
+				Let("up_r", do.Set(me).ToVar(right)).MoveTo("2")).
+			// Put down the fork on his right side
+			Define(rule.At("2").Only(when.Var(right).Is(me)).
+				Let("down_r", do.Set(0).ToVar(right)).MoveTo("3")).
+			// Put down the fork on his left side
+			Define(rule.At("3").Only(when.Var(left).Is(me)).
+				Let("down_l", do.Set(0).ToVar(left)).MoveTo("0"))
+	}
+
+	system := deadlock.NewSystem().
+		Declare(vars.Shared{"f1": 0, "f2": 0}).
+		Register("P1", philo(1, "f1", "f2")).
+		Register("P2", philo(1, "f2", "f1"))
+
+	report, err := deadlock.NewDetector().Detect(system)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+	}
+
+	_, err = deadlock.NewPrinter(os.Stdout).Print(report)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+	}
+
+}
